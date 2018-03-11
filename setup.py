24c1175a94bbb7418a407a1fb9157ac6bdf6f59d
Jay Young <yangjian@patsnap.com>
2018-03-11 11:42:20 +0800

    [INIT]

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# import sys
+from setuptools import setup, find_packages
+
+try:
+    README = open('README.md').read()
+except Exception:
+    README = ""
+VERSION = "0.0.1"
+
+requirments = ["tensorflow"]
+# requirments = ["click", "boto3", "appdirs", "grpcio", "pyyaml", "six"]
+
+# if sys.version_info.major < 3:
+#     requirments += ["configparser", "pathlib"]
+
+setup(
+    name='traffic_gesture_recognition',
+    version=VERSION,
+    description='traffic_gesture_recognition',
+    url="https://github.com/moveithackathon-canhack/traffic-gesture-recognition",
+    long_description=README,
+    author='Jay Young(yjmade), Adam Shan, Dean Zhang, Kunal Tyagi',
+    author_email='carnd@yjmade.net',
+    packages=find_packages(),
+    install_requires=requirments,
+    entry_points={
+        'console_scripts': [
+            # 'modelhub=modelhub.commands:main'
+        ]
+    },
+)
