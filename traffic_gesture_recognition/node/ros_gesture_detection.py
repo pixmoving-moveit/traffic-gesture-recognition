6fc7b4dbf9f3660d2f9f3a9e33f1321efee8bd5b
Kunal Tyagi <tyagi.kunal@live.com>
2018-03-12 19:09:12 +0900

    Adding remaining functionality, testing is left

--- traffic_gesture_recognition/node/ros_gesture_detection.py
+++ traffic_gesture_recognition/node/ros_gesture_detection.py
@@ -4,43 +4,52 @@ import roslib
 roslib.load_manifest('traffic_gesture_recognition')
 
 import rospy
+import rospkg
+import message_filters
 
 from copy import deepcopy
 import numpy as np
 import cv2 as cv
-from Queue import Queue
-
+import os
 
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 
-from autoware_msgs.msg import traffic_light
 from autoware_msgs.msg import image_obj
 
 from traffic_gesture_recognition.msg import police_gesture
+from ..models.inception_v4 import create_inception_v4
 
 
 class GestureRecog:
-
     def __init__(self, debug=False):
-        self.policeman_verifier = create_model(weights="weights.hdf5")
+        package_name = "traffic_gesture_recognition"
+        model_path_prefix = os.path.join(rospack.get_path(package_name), package_name, "models")
+
+        self.policeman_verifier = create_inception_v4(nb_classes=2)
+        self.gesture_dectector = create_inception_v4(nb_classes=2)        
+
+        self.policeman_verifier.load_weights(os.path.join(model_path_prefix, "police_inceptionv4.45-0.28.hdf5"))
+        self.policeman_verifier.load_weights(os.path.join(model_path_prefix, "gesture_inceptionv4.45-0.28.hdf5"))
+
         self.debug = debug
 
         self.gesture_pub = rospy.Publisher("/police_gesture", police_gesture, queue_size=10)
-        self.traffic_light_pub = rospy.Publisher("/light_color", traffic_light, queue_size=10)
         self.gesture_overlay_pub = rospy.Publisher("/police_gesture/image_overlay", Image, queue_size=10)
         
         image_topic_name = "/image_raw"
         param_name = "~image_raw_node"
         if rospy.has_param(param_name):
             image_topic_name = rospy.get_param(param_name)
-        self.image_sub = rospy.Subscriber(image_topic_name, Image, self.raw_img_cb)        
 
-        self.pedestrian_sub = rospy.Subscriber("/obj_person/image_obj", image_obj, self.bb_cb)
+        self.image_sub = message_filters.Subscriber(image_topic_name, Image)
+        self.pedestrian_sub = message_filters.Subscriber("/obj_person/image_obj", image_obj)
+
+        self.queue_size = 15
+        time_sync = message_filters.TimeSynchronizer([self.image_sub, self.pedestrian_sub], self.queue_size)
+        time_sync.registerCallback(self.process)
 
         self.bridge = CvBridge()
-        self.image_queue_size = 15
-        self.incoming_img_q = Queue(self.image_queue_size)
         self.image_to_process = None
         self.incoming_bb = None
 
@@ -50,41 +59,53 @@ class GestureRecog:
     def spin(self):
         rospy.spin()
 
-    def raw_img_cb(self, data):
-        if self.incoming_img_q.full():
-            self.incoming_img_q.get_nowait()
+    def process(self, image, bb):
+        if not data.type == "person":
+            return
         try:
-            self.incoming_img_q.put_nowait(deepcopy(data))
+            self.image_to_process = self.bridge.imgmsg_to_cv2(image, "bgr8")
         except CvBridgeError as e:
             print(e)
             return
 
-    def bb_cb(self, data):
-        if not data.type == "person":
-            return
-        self.incoming_bb = deepcopy(data)
-        if self.incoming_img_q.empty():
-            # This should not occur, ERROR
-            return
-        target_image = None
+        arr = []
+        for box in bb.obj:
+            new_box = self.correct_size(image, box)
+            arr.append(new_box)
+        batch_inp = np.stack(arr, axis=0)
+        batch_result, batch_confidence = predict_policeman(batch_inp)
 
-        while True:
-            target_image = self.incoming_img_q.get_nowait()
-            if target_image.header == self.incoming_bb.header:
-                break
-        self.image_to_process = self.bridge.imgmsg_to_cv2(target_image, "bgr8")
-        self.process(np.copy(self.image_to_process), deepcopy(self.incoming_bb))
+        #assume there's only one policeman
+        indices = [i for i, x in enumerate(batch_result) if x]
+        
+        detected_gesture = police_gesture()
+        detected_gesture.header = bb.header
+        gesture = detect_gesture(arr[indices[0]]) if len(indices) else 0
+
+        self.detected_gesture.gesture = gesture[0]
+        self.detected_gesture.confidence = gesture[1]
+
+        publish_gesture(detected_gesture)
+        if self.debug and len(indices):
+            self.publish_gesture_overlay(image, detected_gesture, bb[indices[0]])
+
+        return detected_gesture
+
+    # img_rect is of type autoware_msgs::image_rect
+    def correct_size(self, image, img_rect):
+        x_right = img_rect.x + img_rect.width
+        y_bottom = img_rect.y + img_rect.height
+        img = image[img_rect.x:x_right, img_rect.y:y_bottom]
+
+        zero_mean = (img - np.mean(img, axis=0))/129=8.
+        scaled_img = cv.resize(img, network_size)
+        return scaled_img
 
     def publish_gesture(self, gesture=None):
         if not gesture:
             gesture = self.detected_gesture
+        # @TODO: check confidence
         self.gesture_pub.publish(gesture)
-
-        tl = traffic_light()
-        tl.header = gesture.header
-        # @TODO
-        tl.color = 1
-        self.traffic_light_pub.publish(tl)
 
     def publish_gesture_overlay(self, image=None, gesture=None, bb=None):
         if not image:
@@ -107,41 +128,20 @@ class GestureRecog:
         msg.header = gesture.header
         self.gesture_overlay_pub.publish(msg)
 
-    def process(self, image=None, bb=None):
-        if not image:
-            image = self.image_to_process
-        if not bb:
-            bb = self.incoming_bb
-        arr = []
-        for box in bb.obj:
-            new_box = self.correct_size(image, box)
-            arr.append(new_box)
-        batch_inp = np.stack(arr, axis=0)
-        batch_result = pedict_policeman(batch_inp)
+    def predict_policeman(self, batch_inp):
+        batch_result = self.policeman_verifier.predict(batch_inp)
+        is_police = np.argmax(batch_result, axis=1)
+        # @TODO: no need to compute twice, do lookup instead
+        confidence = np.max(batch_result, axis=1)
+        return (is_police, confidence)
 
-        batch_result[i] 
+    def detect_gesture(self, inp):
+        result = self.gesture_dectector.predict(inp)
+        gesture = np.argmax(result, axis=1)
+        # @TODO: no need to compute twice, do lookup instead
+        confidence = np.max(result, axis=1)
+        return (gesture, confidence)
 
-        #assume there's only one policeman
-        indices = [i for i, x in enumerate(batch_result) if x]
-        
-        detected_gesture = police_gesture()
-        detected_gesture.header = bb.header
-        detected_gesture.gesture = detect_gesture(arr[indices[0]]) if len(indices) else 0
-
-        self.detected_gesture = detected_gesture
-        publish_gesture(detected_gesture)
-        if self.debug and len(indices):
-            self.publish_gesture_overlay(image, detected_gesture, bb[indices[0]])
-        return detected_gesture
-
-    # img_rect is of type autoware_msgs::image_rect
-    def correct_size(self, image, img_rect):
-        x_right = img_rect.x + img_rect.width
-        y_bottom = img_rect.y + img_rect.height
-        img = image[img_rect.x:x_right, img_rect.y:y_bottom]
-
-        scaled_img = cv.resize(img, network_size)
-        return scaled_img
 
 if __name__ == "__main__":
     ges = GestureRecog(debug=False)
